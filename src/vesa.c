e38d5f46f03c7a8eaba96924810db174cbf2b5e2
b-aaz <85005689+b-aaz@users.noreply.github.com>
2025-09-13 21:52:03 +0000

    vesa.c: Do not check for attached drivers on DFBSD & FBSD
    On both DragonFlyBSD and FreeBSD the vgapci driver is always attach to the device, so we should not dismiss devices based on attached drivers on these platforms.
    
    Signed-off-by: b-aaz <b-aazbsd@proton.me>

--- src/vesa.c
+++ src/vesa.c
@@ -479,10 +479,12 @@ VESAPciProbe(DriverPtr drv, int entity_num, struct pci_device *dev,
     if (pScrn != NULL) {
 	VESAPtr pVesa;
 
+#if !defined (__FreeBSD__) && !defined (__DragonFly__)
 	if (pci_device_has_kernel_driver(dev)) {
 	    ErrorF("vesa: Ignoring device with a bound kernel driver\n");
 	    return FALSE;
 	}
+#endif
 
 	pVesa = VESAGetRec(pScrn);
 	VESAInitScrn(pScrn);
